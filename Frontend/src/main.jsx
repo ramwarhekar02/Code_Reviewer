e9044e2fa5c19606577fcb2a434d4985cab3fb8b
Ram Warhekar <ramwarhekar02@gmail.com>
2025-04-14 22:01:27 +0530

    Lastt

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode, Suspense } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+
+
+const LazyApp = React.lazy(() => import('./App.jsx'));
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Suspense fallback={<div>Loading...</div>}>
+      <LazyApp />
+    </Suspense>
   </StrictMode>,
-)
+);
